37052e84c9dc219c1285182bc7b792d1b89b3e8a
understreck <pqfrml@gmail.com>
2023-03-03 18:58:01 +0100

    Very beginning of file based solution

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
 #include <zlib.h>
 
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iostream>
+
 int
-main() {
-    //zlibVersion();
+main(int argc, char *argv[]) {
+    auto args = std::vector<std::string>{};
+
+    for(int i = 0; i < argc; ++i) {
+        args.emplace_back(argv[i]);
+    }
+
+    auto in = std::ifstream{args[0]};
+
+    in.seekg(0, std::ios_base::end);
+    auto const inSize = in.tellg();
+    in.seekg(0);
+
+    auto outBuf = std::vector<char>{};
+    outBuf.resize(compressBound(inSize));
 }
